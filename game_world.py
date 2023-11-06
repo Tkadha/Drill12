b30ad2a34d8ad3d6bfad30e29e0b16911fd4f52f
Tkadha <emforhs228@naver.com>
2023-11-06 11:38:59 +0900

    충돌 검사 상황 추가

--- game_world.py
+++ game_world.py
@@ -1,11 +1,13 @@
 objects = [[] for _ in range(4)]
 
+
 # fill here
 
-def add_object(o, depth = 0):
+def add_object(o, depth=0):
     objects[depth].append(o)
 
-def add_objects(ol, depth = 0):
+
+def add_objects(ol, depth=0):
     objects[depth] += ol
 
 
@@ -19,6 +21,7 @@ def render():
     for layer in objects:
         for o in layer:
             o.draw()
+
 
 # fill here
 
@@ -36,7 +39,27 @@ def clear():
         layer.clear()
 
 
-
 # fill here
+def collide(a, b):
+    la, ba, ra, ta = a.get_bb()
+    lb, bb, rb, tb = b.get_bb()
+
+    if la > rb: return False
+    if ra < lb: return False
+    if ta < bb: return False
+    if ba > tb: return False
+
+    return True
 
 
+collision_pairs = {}  # {'boy:ball' : [boy], [ball1,ball2,ball3.....] }
+
+
+def add_collision_pair(group, a=None, b=None):  # a 와 b 사이에 충돌 검사가 필요하다는 점을 등록
+    if group not in collision_pairs:
+        print(f'new group {group} add')
+        collision_pairs[group] = [[], []]
+    if a:
+        collision_pairs[group][0].append(a)
+    if b:
+        collision_pairs[group][1].append(b)
